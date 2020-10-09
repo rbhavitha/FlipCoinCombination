85308a7192e71ac2a0c55194579b465ae992309d
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-09 12:27:28 +0530

    Single Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -9,3 +9,37 @@ then
 else
 	echo "Tails"
 fi
+
+declare  -A  coin;
+
+coin[heads]=0;
+coin[tails]=0;
+count=0;
+
+SingleDictionary (){
+	if [ $1 -eq 0 ]
+	then
+		echo "Heads"
+		((coin[heads]++))
+	else
+		
+		echo "Tails"
+		((coin[tails]++))
+	fi
+}
+
+while [ $count -lt 20 ]
+do
+	FlipCoin=$(( $RANDOM%2 ));
+	SingleDictionary $FlipCoin;
+	((count++))
+done
+
+echo "number of times heads:" ${coin[heads]}
+echo "number of times tails:" ${coin[tails]}
+
+percentageofHeads=$(( (${coin[heads]}*100)/20 ))
+percentageofTails=$(( (${coin[tails]}*100)/20 ))
+
+echo "percentage of heads:" $percentageofHeads
+echo "percentage of tails:" $percentageofTails
